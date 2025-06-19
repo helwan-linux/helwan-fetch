83081cef1d051a65850f103e8b926fb439f5ebf1
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-06-19 20:41:33 +0300

    Delete Helfetch0000 directory

--- Helfetch0000/config/default_config.py
+++
@@ -1,8 +0,0 @@
-# config/default_config.py
-
-DEFAULT_COLORS = {
-    "info_key_color": "light_yellow",
-    "info_value_color": "white",
-    "logo_color": "light_cyan",
-    "quote_color": "light_green" # لون جديد للاقتباس
-}
